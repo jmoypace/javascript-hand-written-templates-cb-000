a46a44f2bb98d993ab8c20e726e3e8be68e2ac43
jmoypace <jmoypace@gmail.com>
2018-08-05 15:26:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function addComment() {
   var bodyText = "This research is bold and important!";
   var commenter = "Definitely not a woodchuck looking to eat the wood";
- 
+
   var commentBody = createCommentBody(bodyText);
   var commenterLabel = createCommenterLabel(commenter);
   postNewComment(commentBody, commenterLabel);
 }
- 
+
 function createCommentBody(comment) {
   var bodyDiv = document.createElement("div");
   var bodyPara = document.createElement("p");
@@ -14,7 +14,7 @@ function createCommentBody(comment) {
   bodyDiv.appendChild(bodyPara);
   return bodyDiv;
 }
- 
+
 function createCommenterLabel(commenter) {
   var commenterDiv = document.createElement("div");
   var commenterLabel = document.createElement("p");
@@ -25,7 +25,7 @@ function createCommenterLabel(commenter) {
   commenterDiv.appendChild(commenterLabel);
   return commenterLabel;
 }
- 
+
 function postNewComment(body, commenter) {
   var commentsDiv = document.getElementById("comments");
   var newCommentDiv = document.createElement("div");
